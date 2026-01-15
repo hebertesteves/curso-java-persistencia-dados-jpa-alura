61b10d3368f82f3450adf8fa152de90f99791702
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-14 22:17:51 -0300

    feat: modelando categoria e serie

---
+++ screenmatch/src/main/java/hebertesteves/screenmatch/model/Categoria.java
@@ -0,0 +1,24 @@
+package hebertesteves.screenmatch.model;
+
+public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    public String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+}
