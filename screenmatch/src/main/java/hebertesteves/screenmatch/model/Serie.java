85a08a30ea508e83bdebbe484c4929fb93e9c040
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-22 21:21:29 -0300

    feat: mapeando a entidade de serie

--- screenmatch/src/main/java/hebertesteves/screenmatch/model/Serie.java
+++ screenmatch/src/main/java/hebertesteves/screenmatch/model/Serie.java
@@ -1,15 +1,26 @@
 package hebertesteves.screenmatch.model;
 
 import hebertesteves.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
 
 import java.util.Optional;
-import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
+
     private Integer totalTemporadas;
     private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
+
     private String atores;
     private String poster;
     private String sinopse;
@@ -28,6 +39,14 @@ public class Serie {
         /* Não foi aplicado por questão de credito
             this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
          */
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
